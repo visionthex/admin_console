9f6e513efe4b536e3cddcdf1a58ebbc5c5dbc5bd
visionthex <sandseclos@gmail.com>
2024-01-12 00:49:07 -0500

    Pie Chart

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import Faq from "./scenes/faq";
 import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+
 // import Geography from "./scenes/geography";
 
 
@@ -38,8 +39,8 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<Faq />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/line" element={<Line />} />
 
               <Route path="/geography" element={<Geography />} /> */}
 
