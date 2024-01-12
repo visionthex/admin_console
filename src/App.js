c619e65d788c04afc5b3c9231dab4e42739b21be
visionthex <sandseclos@gmail.com>
2024-01-12 00:31:00 -0500

    Bar Chart

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import Faq from "./scenes/faq";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
@@ -37,8 +37,8 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<Faq />} />
-              {/* <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
+              <Route path="/bar" element={<Bar />} />
+              {/* <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
 
               <Route path="/geography" element={<Geography />} /> */}
